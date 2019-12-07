06446659b6a4b00b8c3aa9dd1c8551a1c7ad4b50
Ran Levy <RanLevy62@gmail.com>
2019-12-07 21:15:51 +0200

    http

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print ('Hello')
+import requests
+import json
+
+#response = requests.get('https://www.thebluealliance.com/api/v3/team/frc5990?X-TBA-Auth-Key=ptM95D6SCcHO95D97GLFStGb4cWyxtBKNOI9FX5QmBirDnjebphZAEpPcwXNr4vH')
+response = requests.get('https://www.thebluealliance.com/api/v3/team/frc5990', headers = {'X-TBA-Auth-Key': 'ptM95D6SCcHO95D97GLFStGb4cWyxtBKNOI9FX5QmBirDnjebphZAEpPcwXNr4vH'})
+#string = response.read().decode('utf-8')
+json_obj = json.loads(response.content)
+
+print(json_obj['city']) # prints the string with 'source_name' key
